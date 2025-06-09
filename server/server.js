0693c3db74ebf5777a8f3df4c182c3f858e17a8c
tejasmishra1701 <tejasmishralar.3017@gmail.com>
2025-06-09 19:28:50 +0530

    deployment

--- server/server.js
+++ server/server.js
@@ -16,8 +16,10 @@ let db;
 // MongoDB Connection
 async function connectToDatabase() {
   try {
-    const client = await MongoClient.connect(MONGODB_URI);
-    db = client.db('mintyscan');
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log('Connected to MongoDB successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
